244af70d82230c67482d425c648463fad4756584
Mohamed Tarek <mohamedtarekabdelhamid@gmail.com>
2025-11-14 23:43:21 +0200

    feat: update dependencies and enhance Navbar component
    - Added new dependencies: @headlessui/react, @heroicons/react, antd, and lucide-react.
    - Introduced a new favicon.ico file.
    - Removed vite.svg file.
    - Enhanced Navbar component with a logo, navigation links, and a language dropdown using Ant Design.
    - Updated global styles in index.css for improved layout and appearance.

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,76 @@
 import React from 'react';
+import { Button, Dropdown, Space } from 'antd';
+
+import logo from '../../assets/images/logo.png';
+import { ChevronDown } from 'lucide-react';
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const items = [
+        {
+            key: '1',
+            label: (
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.antgroup.com"
+                >
+                    English
+                </a>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.aliyun.com"
+                >
+                    عربي
+                </a>
+            ),
+        },
+    ];
+    return (
+        <div>
+            <nav className="my-10">
+                <div className="container mx-auto flex justify-between items-center text-[#3d3d3d]">
+                    <div className="navbar-logo flex items-center">
+                        <img src={logo} alt="" />
+                        <h1 className="text-xl font-bold ml-4">Pocket AI</h1>
+                    </div>
+                    <div className="font-normal text-sm">
+                        <a href="/" className="px-3">
+                            Features
+                        </a>
+                        <a href="/about" className="px-3">
+                            How it Works
+                        </a>
+                        <a href="/contact" className="px-3">
+                            FAQ
+                        </a>
+                        <a href="/contact" className="px-3">
+                            Download
+                        </a>
+                    </div>
+                    <div>
+                        <Space wrap>
+                            <Dropdown
+                                menu={{ items }}
+                                placement="bottom"
+                                arrow={{ pointAtCenter: true }}
+                            >
+                                <button className="text-base text-[#3D3D3D] bg-[#F4F4F4] border-[1px] border-[#ECECEC] px-9 py-3 rounded-full hover:bg-[#e0e0e0] transition duration-300">
+                                    English{' '}
+                                    <ChevronDown className="inline-block" />
+                                </button>
+                            </Dropdown>
+                        </Space>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;
