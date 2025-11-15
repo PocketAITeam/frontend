37a246590683bed0a5426d5ef0bc3b83fcfb5bf2
Mohamed Tarek <mohamedtarekabdelhamid@gmail.com>
2025-11-15 19:07:28 +0200

    feat: implement Title component with section title and underline image

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Title = () => {
-    return <div>Title</div>;
+import underline from '../../assets/images/underline-lg.svg';
+
+const Title = ({ sectionTitle }) => {
+    return (
+        <div className="flex flex-col items-center">
+            <h2 className="text-[#292929] font-bold text-[40px] mt-12">
+                {sectionTitle}
+            </h2>
+            <div className="mt-1.5 mb-12 ml-32">
+                <img src={underline} alt="" />
+            </div>
+        </div>
+    );
 };
 
 export default Title;
